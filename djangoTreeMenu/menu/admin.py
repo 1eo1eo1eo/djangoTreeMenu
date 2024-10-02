37cc79464865d2620d62fd4198bb03bc9c97da69
Lev <lafitel31@gmail.com>
2024-10-02 19:01:16 +1000

    register Menu and MenuItem models

--- djangoTreeMenu/menu/admin.py
+++ djangoTreeMenu/menu/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from menu.models import Menu, MenuItem
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "url",
+        "parent",
+    ]
+
+
+class MenuItemTabularAdmin(admin.TabularInline):
+    model = MenuItem
+    extra = 1
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = [MenuItemTabularAdmin]
