6264e221f3d8679f3a708608898eaed0e6101e21
Lev <lafitel31@gmail.com>
2024-10-02 18:59:59 +1000

    register menu app and host from "0.0.0.0" to "db"

--- djangoTreeMenu/djangoTreeMenu/settings.py
+++ djangoTreeMenu/djangoTreeMenu/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "menu",
 ]
 
 MIDDLEWARE = [
@@ -79,7 +80,7 @@ DATABASES = {
         "NAME": "djangoTreeMenu",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "0.0.0.0",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
