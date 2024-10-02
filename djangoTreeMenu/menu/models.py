7e45e681727dd2e9612c1d5d31538782112b82ba
Lev <lafitel31@gmail.com>
2024-10-02 19:01:06 +1000

    create Menu and MenuItem models

--- djangoTreeMenu/menu/models.py
+++ djangoTreeMenu/menu/models.py
@@ -1,3 +1,38 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    named_url = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="children",
+    )
+    menu = models.ForeignKey(
+        to=Menu,
+        related_name="items",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
